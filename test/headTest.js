22f73bd2e052835476e4728be2e966ecb4387e7a
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-11-21 11:07:21 +0000

    new testing head

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,31 @@
 const head = require("../head");
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+
+
+describe("#head", () => {
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns [] for undefined", () => {
+    assert.strictEqual(head([undefined]), undefined);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+});
 
 
 
-console.log(assertEqual(head([]), undefined));
-console.log(assertEqual(head([5]), 5));
-console.log(assertEqual(head([5, 6, 7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+
+
+// console.log(assertEqual(head([]), undefined));
+// console.log(assertEqual(head([5]), 5));
+// console.log(assertEqual(head([5, 6, 7]), 5));
+// console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
