105d7280b7db6fc7e6b976ce2caa738063c80fe9
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-11-21 11:34:14 +0000

    tail update

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,23 @@
 const tail = require("../tail");
 const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
 
-const nothing = tail([]);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
+// const nothing = tail([]);
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
 
-console.log(assertEqual(nothing.length, undefined));
-console.log(assertEqual(result.length, 2));
-console.log(assertEqual(result[0], "Lighthouse"));
+// console.log(assertEqual(nothing.length, undefined));
+// console.log(assertEqual(result.length, 2));
+// console.log(assertEqual(result[0], "Lighthouse"));
+
+
+describe("#tail", () => {
+  it("returns undefined for empty array", () => {
+    assert.strictEqual(tail([]), undefined);
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs" ]);
+  });
+})
+
