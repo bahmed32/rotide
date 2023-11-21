32e6bebb227e642fada408ff5d27fc28281978e5
Bilqis Ahmed <bilqis.ali32@gmail.com>
2023-11-21 11:23:49 +0000

    middle update

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,4 @@
-const eqArrays = require("../eqArrays");
-const assertArraysEqual = require("../assertArraysEqual");
+const assert = require('chai').assert;
 const middle = require("../middle");
 
 console.log(middle([1])); // => []
@@ -7,3 +6,19 @@ console.log(middle([1, 2]));
 
 console.log(middle([1, 2, 3, 4]));
 console.log(middle([1, 2, 3, 4, 5]));
+
+describe("# middle", () => {
+
+  it(" return [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]),[3]);
+  });
+
+  it(" return [2,3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it(" return 'Please add more inputs into array' for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), "Please add more inputs into array");
+  });
+
+});
